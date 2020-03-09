c4963c77c3180bca433f17873c2f8dbb0f17220e
Meagle <mindfulyeon@gmail.com>
2020-03-09 13:11:10 +0900

    리덕스 현재 스테이트를 변경해 리턴, 액션을 디스패치하는 법

--- src/index.js
+++ src/index.js
@@ -5,10 +5,27 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 // 스테이트가 존재하지 않는다면 0 으로 스테이트 초기화함.
-const countModifier = (state = 0) => {
-  return state;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
+// countModifier에게 count++를 리턴하라고 어떻게 말할 수 있을까? => 액션이 하는 일
+
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
