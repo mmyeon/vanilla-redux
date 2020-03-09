93bf33014527a0b1c39e3bcf5b7c80503f6d1538
Meagle <mindfulyeon@gmail.com>
2020-03-09 16:08:36 +0900

    if 조건문 switch문으로 수정, 액션타입을 읽을 수 있는 const 사용

--- src/index.js
+++ src/index.js
@@ -7,14 +7,18 @@ const number = document.querySelector("span");
 // 처음 시작
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 // 스테이트가 존재하지 않는다면 0 으로 스테이트 초기화함.
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -29,10 +33,10 @@ const onChange = () => {
 countStore.subscribe(onChange); // 스테이트가 스토어에서 바뀔때마다 onChange함수 호출됨
 
 const handleAdd = () => {
-  return countStore.dispatch({ type: "ADD" });
+  return countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  return countStore.dispatch({ type: "MINUS" });
+  return countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
