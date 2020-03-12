5f42c3b24572d8cac432b96a90142a945e5fad2f
Meagle <mindfulyeon@gmail.com>
2020-03-12 17:58:06 +0900

    Detail 페이지 이동

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,27 @@
-export default () => "Detail";
+import React from "react";
+// import { useParams } from "react-router-dom";
+import { connect } from "react-redux";
+
+function Detail({ toDo }) {
+  //   console.log(props);
+  //   const id = useParams();
+  //   console.log(id);
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+      <h2>Created at : {toDo?.id}</h2>
+    </>
+  );
+}
+
+function mapStateToProps(state, ownProps) {
+  const {
+    match: {
+      params: { id }
+    }
+  } = ownProps;
+  //   console.log(id);
+  return { toDo: state.find(toDo => toDo.id === parseInt(id)) };
+}
+
+export default connect(mapStateToProps)(Detail);
