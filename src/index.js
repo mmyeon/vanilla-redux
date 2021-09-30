ef3d27126b6701040dd14c413dba04cfdca459d5
mmyeon <mindfulyeon@gmail.com>
2021-10-01 00:02:39 +0900

    리덕스 스토어, 리듀서, 디스패치 코드 추가

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
+import { createStore } from "redux";
+
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createToDo = (toDo) => {
-  const li = document.createElement("li");
-  li.innerText = toDo;
-  ul.appendChild(li);
+const ADD_TO_DO = "ADD_TO_DO";
+const DELETE_TO_DO = "DELETE_TO_DO";
+
+const reducer = (state = [], action) => {
+  console.log(action);
+  switch (action.type) {
+    case ADD_TO_DO:
+      return [];
+    case DELETE_TO_DO:
+      return [];
+    default:
+      return state;
+  }
 };
+
+const store = createStore(reducer);
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  console.log(toDo);
-  createToDo(toDo);
+  store.dispatch({ type: ADD_TO_DO, text: toDo });
 };
 
 form.addEventListener("submit", onSubmit);
