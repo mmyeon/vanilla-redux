f73160e1fdfe264e8e24ee59da066856cd0da9da
mmyeon <mindfulyeon@gmail.com>
2021-10-05 16:36:29 +0900

    [feat] DELETE 액션을 디스패치해서 투두 삭제기능 추가

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-const ToDo = ({ text }) => {
+const ToDo = ({ text, onBtnClick }) => {
   return (
     <li>
       {text}
-      <button>DEL</button>
+      <button onClick={onBtnClick}>DEL</button>
     </li>
   );
 };
 
-export default ToDo;
+function mapDispatchToProps(dispatch, ownProps) {
+  return { onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)) };
+}
+
+export default connect(null, mapDispatchToProps)(ToDo);
