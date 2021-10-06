c4fc44223a6ff3e7b7b32d7fbfd493309a35b6bd
mmyeon <mindfulyeon@gmail.com>
2021-10-06 11:21:50 +0900

    [feat] 상세페이지 삭제 기능 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-const Detail = ({ toDo }) => {
+const Detail = ({ toDo, onBtnClick }) => {
   return (
     <>
       <h1>{toDo?.text}</h1>
+      <button onClick={onBtnClick}>DEL</button>
       <h5>Created at :{toDo?.id}</h5>
     </>
   );
 };
+
+function mapDispatchToProps(dispatch, ownProps) {
+  const {
+    match: {
+      params: { id },
+    },
+  } = ownProps;
+
+  return {
+    onBtnClick: () => dispatch(actionCreators.deleteToDo(parseInt(id))),
+  };
+}
 
 function mapStateToProps(state, ownProps) {
   const {
@@ -22,4 +36,4 @@ function mapStateToProps(state, ownProps) {
   };
 }
 
-export default connect(mapStateToProps)(Detail);
+export default connect(mapStateToProps, mapDispatchToProps)(Detail);
