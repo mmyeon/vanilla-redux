f89adb2cf7704ae81da2107c6a06d982e5081240
mmyeon <mindfulyeon@gmail.com>
2021-10-06 11:50:04 +0900

    [refactor] 리덕스 툴킷 createAction함수로 코드 가독성 개선

--- src/store.js
+++ src/store.js
@@ -1,29 +1,16 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id,
-  };
-};
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      const newToDoObj = { text: action.text, id: Date.now() };
+    case addToDo.type:
+      const newToDoObj = { text: action.payload, id: Date.now() };
       return [newToDoObj, ...state];
-    case DELETE:
-      const cleaned = state.filter((toDo) => toDo.id !== action.id);
+    case deleteToDo.type:
+      const cleaned = state.filter((toDo) => toDo.id !== action.payload);
       return cleaned;
     default:
       return state;
